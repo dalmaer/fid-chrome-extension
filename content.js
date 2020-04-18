42b47618e951e5ebd865792575d25dfaff562d2b
Dion Almaer <dion@almaer.com>
2020-04-17 19:12:35 -0600

    guard against double inclusion of the content script

--- content.js
+++ content.js
@@ -1,14 +1,17 @@
-// Create the PerformanceObserver instance.
-const po = new PerformanceObserver((entryList) => {
-  for (const entry of entryList.getEntries()) {
-    // Send the FID to the background page
-    let fid = Math.round(entry.processingStart - entry.startTime);
-    chrome.runtime.sendMessage({ result: fid });
-    console.log("FID:", fid, "ms");
-    // po.disconnect();
-  }
-});
+// guard against double-inclusion
+if (typeof po == "undefined") {
+  // Create the PerformanceObserver instance.
+  const po = new PerformanceObserver((entryList) => {
+    for (const entry of entryList.getEntries()) {
+      // Send the FID to the background page
+      let fid = Math.round(entry.processingStart - entry.startTime);
+      chrome.runtime.sendMessage({ result: fid });
+      console.log("FID:", fid, "ms");
+      // po.disconnect();
+    }
+  });
 
-// Observe entries of type `paint`, including buffered
-// entries, i.e. entries that occurred before calling `observe()`.
-po.observe({ type: "first-input", buffered: true });
+  // Observe entries of type `paint`, including buffered
+  // entries, i.e. entries that occurred before calling `observe()`.
+  po.observe({ type: "first-input", buffered: true });
+}
